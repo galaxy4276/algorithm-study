e73cebc71f9e5bb9fce7303309d090e2b36df1b7
최은기 <galaxyhi4276@gmail.com>
2022-07-16 20:55:34 +0900

    feat: playground rst update

--- playground/galaxy4276/src/main.rs
+++ playground/galaxy4276/src/main.rs
@@ -1,18 +1,18 @@
 // [3,2,4]
 fn main() {
-    let vec = vec![0, 2, 4, 6];
-
-    two_sum(vec, 6);
+    let b: bool = is_palindrome(-121);
 }
 
-fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    for i in 0..nums.len() {
-        for j in i + 1..nums.len() {
-            if nums[i] + nums[j] == target {
-                return vec![i as i32, j as i32];
-            }
+fn is_palindrome(x: i32) -> bool {
+    if x < 0 {
+        return false;
+    }
+    let chars: Vec<char> = x.to_string().chars().collect();
+    let reversed_chars: Vec<char> = x.to_string().chars().rev().collect();
+    for idx in 0..chars.len() {
+        if chars[idx] != reversed_chars[idx] {
+            return false;
         }
     }
-
-    unreachable!();
+    true
 }
