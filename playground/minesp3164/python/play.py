3cbf6c777622507818ea898ef3f9f9fdec17e389
최은기 <galaxyhi4276@gmail.com>
2022-07-16 18:51:53 +0900

    feat: minesp 용 python playground 추가

---
+++ playground/minesp3164/python/play.py
@@ -0,0 +1,10 @@
+# 현재 코드 실행문 ( project root 기준 )
+# python playground/minesp3164/python/play.py
+
+class Solution:
+  # 문제에서 제시하는 메서드를 여기에 copy & paste 하기
+
+if __name__ == "__main__":
+  solution = Solution()
+  # 여기에서 입력 데이터 예시를 작성하고, Solution 클래스 메서드를 호출하여 인자를 입력
+  # 예시 ) solution.two_sum(data, data)  
