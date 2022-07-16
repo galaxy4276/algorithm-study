fac833ca8c491d747a53519874918d37d83946f1
최은기 <galaxyhi4276@gmail.com>
2022-07-16 19:20:10 +0900

    fix: two sum(1) playground 잘못 반영된 내용 복구

--- easy-solution/galaxy4276/1.rs
+++ easy-solution/galaxy4276/1.rs
@@ -1,13 +1,12 @@
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        for (i_idx, i_num) in nums.iter().enumerate() {
-            for (j_idx, j_num) in nums.iter().enumerate() {
-                if i_num + j_num == target {
-                    return vec![i_idx as i32, j_idx as i32];
+        for i in 0..nums.len() {
+            for j in i + 1..nums.len() {
+                if nums[i] + nums[j] == target {
+                    return vec![i as i32, j as i32];
                 }
             }
         }
-
         unreachable!();
     }
 }
