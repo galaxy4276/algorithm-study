ef7df9b3e992ec1b8fa1639e44b148cca3af921f
최은기 <galaxyhi4276@gmail.com>
2022-07-21 19:38:26 +0900

    solve - strStr(28) python
    fucking

--- playground/galaxy4276/src/main.rs
+++ playground/galaxy4276/src/main.rs
@@ -1,27 +1,42 @@
 fn main() {
-    let strs = vec!["dog".to_string(), "racecar".to_string(), "car".to_string()];
-    let strs2 = vec!["cir".to_string(), "car".to_string()];
-    let res: String = longest_common_prefix(strs2);
+    let test_case1 = "()[]{}".to_string();
+    let test_case2 = "{[]}".to_string();
+    let res = is_valid(test_case2);
     println!("res: {}", res);
 }
 
-fn longest_common_prefix(strs: Vec<String>) -> String {
-    let mut first: String = strs[0].to_string();
-
-    for i in 1..strs.len() {
-        let mut calc_s = "".to_string();
-        let mut switch = true;
-        for (j, s) in strs[i].chars().enumerate() {
-            let last_bytes = *&first.as_bytes();
-            if switch && j < last_bytes.len() && last_bytes[j] as char == s {
-                calc_s.push(s);
-            } else {
-                switch = false;
-            }
+pub fn is_valid(s: String) -> bool {
+    let mut stack  = vec![];
+    for c in s.chars() {
+        match c {
+            ')' => match stack.pop() {
+                Some(c) => {
+                    if c != '(' {
+                        return false;
+                    }
+                }
+                None => return false,
+            },
+            ']' => match stack.pop() {
+                Some(c) => {
+                    if c != '[' {
+                        return false;
+                    }
+                },
+                None => return false,
+            },
+            '}' => match stack.pop() {
+                Some(c) => {
+                    if c != '{' {
+                        return false;
+                    }
+                },
+                None => return false,
+            },
+            _ => {
+                stack.push(c);
+            },
         }
-        if calc_s.len() == 0 { return "".to_string() }
-        first = calc_s;
     }
-
-    first
+    return stack.is_empty();
 }
