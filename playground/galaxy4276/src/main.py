16fb2e868cd1f773bb6c8ade5073b60063a2eb9c
최은기 <galaxyhi4276@gmail.com>
2022-07-24 19:51:04 +0900

    solve: Implement strStr()(28) rust lang

---
+++ playground/galaxy4276/src/main.py
@@ -0,0 +1,4 @@
+def strStr(haystack: str, needle: str) -> int:
+  if needle not in haystack
+    return -1
+  return haystack.find(needle)
--- playground/galaxy4276/src/main.rs
+++ playground/galaxy4276/src/main.rs
@@ -1,42 +1,14 @@
 fn main() {
-    let test_case1 = "()[]{}".to_string();
-    let test_case2 = "{[]}".to_string();
-    let res = is_valid(test_case2);
-    println!("res: {}", res);
+  let haystack = "hello".to_string();
+  let needle = "ll".to_string();
+
+  let result = str_str(haystack, needle);
+  println!("result: {}", result);
 }
 
-pub fn is_valid(s: String) -> bool {
-    let mut stack  = vec![];
-    for c in s.chars() {
-        match c {
-            ')' => match stack.pop() {
-                Some(c) => {
-                    if c != '(' {
-                        return false;
-                    }
-                }
-                None => return false,
-            },
-            ']' => match stack.pop() {
-                Some(c) => {
-                    if c != '[' {
-                        return false;
-                    }
-                },
-                None => return false,
-            },
-            '}' => match stack.pop() {
-                Some(c) => {
-                    if c != '{' {
-                        return false;
-                    }
-                },
-                None => return false,
-            },
-            _ => {
-                stack.push(c);
-            },
-        }
-    }
-    return stack.is_empty();
+fn str_str(haystack: String, needle: String) -> i32 {
+  match haystack.find(&needle) {
+    Some(n) => n as i32,
+    None => -1,
+  }
 }
