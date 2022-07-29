5f231674b89a5c007c969fba5dec15ac939a9963
최은기 <galaxyhi4276@gmail.com>
2022-07-29 16:07:08 +0900

    solve:  Remove Duplicates from Sorted List (83) python lang

--- easy-solution/galaxy4276/83.py
+++ easy-solution/galaxy4276/83.py
@@ -1,7 +1,9 @@
 class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+  def __init__(self, val=0, next=None):
+    self.val = val
+    self.next = next
+
+
 class Solution:
   def deleteDuplicates(self, head):
     dulList = []
