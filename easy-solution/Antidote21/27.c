f0d67b15e59f1ee9929ef8cc53a6395c50ab956e
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2022-08-08 22:00:17 +0900

    Update 27.c

--- easy-solution/Antidote21/27.c
+++ easy-solution/Antidote21/27.c
@@ -1,1 +1,12 @@
 
+
+int removeElement(int* nums, int numsSize, int val){
+    int i,index=0;
+    for(i=0;i<numsSize;i++){
+        if(nums[i]!=val){
+            nums[index++] = nums[i];
+        }
+    }
+    return index;
+}
+   
