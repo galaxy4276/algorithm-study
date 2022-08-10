0fe15f1a868fcc3f5c6ffcf9a45b0545c7988373
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2022-08-10 23:26:08 +0900

    Update 1346.c

--- easy-solution/Antidote21/1346.c
+++ easy-solution/Antidote21/1346.c
@@ -13,8 +13,8 @@ bool checkIfExist(int* arr, int arrSize){
     }
     
     for(i=0;i<arrSize;i++){
-        for(j=0;j<arrSize;j++){
-            if(arr[i] == 2*arr[j]&&arr[i]!=0){
+        for(j=0;j<arrSize;j++){ //j=1로 변경해야 함 
+            if(arr[i] == 2*arr[j]&&arr[i]!=0){ //arr[i] == 2*arr[j] ||  arr[j] == 2 * arr[i] 0이 두 개 일때도 성립 가능 
                 return true;
             }
         }
@@ -23,3 +23,12 @@ bool checkIfExist(int* arr, int arrSize){
     
     return false;
 }
+
+bool checkIfExist(int* arr, int arrSize){
+        for (int i = 0; i < arrSize; i++){
+            for (int j = i+1; j < arrSize; j++){
+                if(arr[i] == 2*arr[j] ||  arr[j] == 2 * arr[i]) return true;
+            }
+        }
+        return false;
+    }
