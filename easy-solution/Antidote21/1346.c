9f5ad125d23194063971c8c89deb71f992084b07
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2022-08-11 21:11:09 +0900

    solve: Check If N and Its Double Exist(1346)C lang

--- easy-solution/Antidote21/1346.c
+++ easy-solution/Antidote21/1346.c
@@ -27,7 +27,7 @@ bool checkIfExist(int* arr, int arrSize){
 bool checkIfExist(int* arr, int arrSize){
         for (int i = 0; i < arrSize; i++){
             for (int j = i+1; j < arrSize; j++){
-                if(arr[i] == 2*arr[j] ||  arr[j] == 2 * arr[i]) return true;
+                if(arr[i] == 2*arr[j] ||  arr[j] == 2 * arr[i]) return true; 
             }
         }
         return false;
