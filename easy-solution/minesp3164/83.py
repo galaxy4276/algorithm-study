2d709eca92f5b5565cf458b489c02f072dc0580f
minesp <lhd000721@gmail.com>
2022-08-14 23:30:35 +0900

    update wrong  code

--- easy-solution/minesp3164/83.py
+++ easy-solution/minesp3164/83.py
@@ -7,10 +7,27 @@ class Solution(object):
     def deleteDuplicates(self, head):
         fast = head.next
         slow = head
-        while slow.next:
+        while fast.next:
             if fast.val == slow.val:
                 slow.next = fast.next
                 fast = slow.next
             else:
                 fast, slow = fast.next, slow.next
         return head
+"""
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        list =  ListNode(0)
+        sumlist = list
+        while list1 and list2 :
+            if list1.val <= list2.val:
+                sumlist.next = list1
+                list1 = list1.next
+            else :
+                sumlist.next = list2
+                list2 = list2.next
+            sumlist = sumlist.next
+        sumlist.next = list1 or list2
+
+        return sumlist.next
+"""
