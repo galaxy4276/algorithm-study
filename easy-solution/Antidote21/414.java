8f22a9841aac5667d026193a515fef42852acf13
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2022-08-17 00:10:49 +0900

    solve: Third Maximum Number(414) java lang

--- easy-solution/Antidote21/414.java
+++ easy-solution/Antidote21/414.java
@@ -1,1 +1,20 @@
-414. Third Maximum Number
+class Solution {
+    public int thirdMax(int[] nums) {
+        Arrays.sort(nums);
+        int prev = nums[0];
+        int index = 1;
+        for (int i=1; i<nums.length; i++) {
+            if (prev == nums[i]) {
+                continue;
+            }
+            prev = nums[i];
+            nums[index++] = nums[i]; 
+        }
+
+        if (index < 3) {
+            return nums[index-1];
+        }
+        
+        return nums[index-3];
+    }
+}
