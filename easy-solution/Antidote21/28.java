7579882cf087afa89f993c9c85a0387756cd003f
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2022-08-26 23:58:26 +0900

    solve: Implement strStr(28) java lang

--- easy-solution/Antidote21/28.java
+++ easy-solution/Antidote21/28.java
@@ -2,6 +2,6 @@ class Solution {
     public int strStr(String haystack, String needle) {
         int index=0;
         index = haystack.indexOf(needle);
-        return index;
+        return index; 
     }
 }
