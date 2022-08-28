974408e9d81aa5dba2b263e3f65cd6d66b882d80
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2022-08-28 20:18:45 +0900

    retry: Minimum Size Subarray Sum(209) java lang

--- easy-solution/Antidote21/209.java
+++ easy-solution/Antidote21/209.java
@@ -1,37 +1,19 @@
 class Solution{
-    public int minSubArrayLen(int target, int[] nums) {
-        if(nums==null || nums.length==1)
-            return 0;
+   public int minSubArrayLen(int s, int[] a) {
+      if (a == null || a.length == 0)
+        return 0;
 
-        int result = nums.length;
+      int i = 0, j = 0, sum = 0, min = Integer.MAX_VALUE;
 
-        int start=0;
-        int sum=0;
-        int i=0;
-        boolean exists = false;
+      while (j < a.length) {
+        sum += a[j++];
 
-        while(i<=nums.length){
-            if(sum>=target){
-                exists=true; //mark if there exists such a subarray 
-                if(start==i-1){
-                    return 1;
-                }
-
-                result = Math.min(result, i-start);
-                sum=sum-nums[start];
-                start++;
-
-            }else{
-                if(i==nums.length)
-                    break;
-                sum = sum+nums[i];
-                i++;    
-            }
+        while (sum >= s) {
+          min = Math.min(min, j - i);
+          sum -= a[i++];
         }
-
-        if(exists)
-            return result;
-        else
-            return 0;
+      }
+      
+      return min == Integer.MAX_VALUE ? 0 : min;
     }
 }
