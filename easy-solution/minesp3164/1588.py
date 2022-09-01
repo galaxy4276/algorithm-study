3b5da59b95c6e3a36142bfad581c72d481c44b1e
minesp <lhd000721@gmail.com>
2022-09-01 18:03:39 +0900

    solve:Sum of All Odd Length Subarrays(1588) python lang

---
+++ easy-solution/minesp3164/1588.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sumOddLengthSubarrays(self, arr):
+        total = 0
+        for i in range(1,len(arr)+1,2):
+            j = 0
+            count = i
+            while count <= len(arr):
+                total+=sum(arr[j:count])
+                j+=1
+                count+=1
+        return total
