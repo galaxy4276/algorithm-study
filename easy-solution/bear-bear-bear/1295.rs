640f5062e5a261e60faf35b6ddf4debe75cd49c3
Will Kim <will@chequer.io>
2022-09-11 02:55:14 +0900

    Add improved solution

--- easy-solution/bear-bear-bear/1295.rs
+++ easy-solution/bear-bear-bear/1295.rs
@@ -8,4 +8,13 @@ impl Solution {
         }
         count
     }
+
+
+    // https://leetcode.com/problems/find-numbers-with-even-number-of-digits/discuss/457571/Python3-C%2B%2B11-C%2B%2B20-Java-Haskell-Clojure-Rust-Scala-Elixir-and-APL-Solutions
+    pub fn refactored_find_numbers(nums: Vec<i32>) -> i32 {
+        return nums.iter()
+            .map(|i| i.to_string().len())
+            .filter(|i| i % 2 == 0)
+            .count() as i32;
+    }
 }
