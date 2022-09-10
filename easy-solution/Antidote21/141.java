ca5b3f633bfc3b36bdae2f35a938498f9d2d528b
Antidote21 <91773314+Antidote21@users.noreply.github.com>
2022-09-10 21:18:41 +0900

    retry: Linked List Cycle(141) java lang

---
+++ easy-solution/Antidote21/141.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ArrayList<ListNode> arraylist = new ArrayList();
+        if (head == null){
+            return false;
+        }
+        if (head.next == null){
+            return false;
+        }
+        
+        while(true){
+            arraylist.add(head);
+            head = head.next;
+            
+            for(int i  = 0 ; i < arraylist.size() ; i++){
+                if (arraylist.get(i) == head){
+                    return true;
+                }
+            }
+            
+            if (head.next == null){
+                break;
+            }
+        }
+        return false;
+        
+    }
+}
