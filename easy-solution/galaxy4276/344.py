9cf7b167ca1f29f88a1c34908494e6d13b5ef593
최은기 <galaxyhi4276@gmail.com>
2022-11-12 22:41:47 +0900

    solve: [Reorder Data in Log Files] python
    https://leetcode.com/problems/reorder-data-in-log-files/description/

--- easy-solution/galaxy4276/344.py
+++ easy-solution/galaxy4276/344.py
@@ -2,12 +2,17 @@ from typing import List
 
 
 class Solution:
-    def reverse_string_two_pointer(self, s: List[str]) -> None:
-        first, last = 0, len(s) - 1
-        while first < last:
-            s[first], s[last] = s[last], s[first]
-            first += 1
-            last -= 1
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        letters, digits = [], []
+        for log in logs:
+            if log.split()[1].isdigit():
+                digits.append(log)
+            else:
+                letters.append(log)
+        letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+        return letters + digits
 
-    def reverse_string_pythonic(self, s: List[str]) -> None:
-        s.reverse()
+if __name__ == '__main__':
+    solution = Solution()
+    result: List[str] = solution.reorderLogFiles(["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"])
+    print(result)
