84681fc5263c792f8cf90ec7ec1ae7b8c09825f4
Baek DongJae <pandora2948@naver.com>
2022-12-21 15:14:58 +0900

    feat [circularQueue] python
    (cherry picked from commit de7554062f9a43493d241df928ac1e1dec1bb1ee)

---
+++ playground/pandora2948/circularQueue/__init__.py
@@ -0,0 +1,45 @@
+class CircularQueue:
+
+    def __init__(self, k: int):
+        self.data = [0 for _ in range(k + 1)]
+        self.MAX_SIZE = k + 1
+        self.front = 0
+        self.rear = 0
+
+    def enQueue(self, value: int) -> bool:
+        if not self.isFull():
+            self.data[self.rear] = value
+            self.rear = (self.rear + 1) % self.MAX_SIZE
+            return True
+        return False
+
+    def deQueue(self) -> bool:
+        if not self.isEmpty():
+            self.data[self.front] = 0
+            self.front = (self.front + 1) % self.MAX_SIZE
+            return True
+        return False
+
+    def Front(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.data[(self.front + self.MAX_SIZE - 1) % self.MAX_SIZE]
+    def Rear(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.data[(self.rear + self.MAX_SIZE - 1) % self.MAX_SIZE]
+
+    def isEmpty(self) -> bool:
+        return self.rear == self.front
+
+    def isFull(self) -> bool:
+        return (self.rear + 1) % self.MAX_SIZE == self.front
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
