18ce00c976d9a4ee104103dd99209ed7ff79d5fb
minesp3164 <lhd000721@gmail.com>
2023-07-11 20:27:24 +0900

    solved: [Number of Senior Citizens] python

---
+++ easy-solution/minesp3164/python/2678.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def countSeniors(self, details):
+        count = 0
+        for i in details:
+            if int(i[11:13]) > 60:
+                count += 1
+        return count
